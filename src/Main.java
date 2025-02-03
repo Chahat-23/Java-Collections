19276cc7f9f4aa7c8467bad8cc64205e4cfe40ee
Chahat-23 <chahatshrivastava230620@gmail.com>
2025-02-03 17:00:29 +0530

    Java Collections

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        var list = new GenericList<String>();
+        list.add("a");
+        list.add("b");
+        for (var item: list.items) {
+            System.out.println(item);
         }
     }
 }
